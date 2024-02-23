e0ff7a4831de1304caa1d86dd906d89b81e3bab8
skanderkaroui <skander.karoui@gmail.com>
2024-02-23 16:59:54 +0100

    reset the solution

--- 3_arrays.py
+++ 3_arrays.py
@@ -1,11 +1,6 @@
-string ="abccddelkjr"
+# def remove_duplicates(input_str):
 
-# def rem_dup(string):
-#     set_string = set(string)
-#     rm_string = "".join(set_string)
-#     return rm_string
-
-#wrong solution because it uses set (additional buffer means either a set or a list)
-
-
-print(rem_dup(string))
+# Example usage:
+my_string = "aaaaaaaaaaabc"
+result = remove_duplicates(my_string)
+print(result)
