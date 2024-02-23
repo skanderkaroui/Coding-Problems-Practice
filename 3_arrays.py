7258b13a7104086eee0f6d5c0de1b51fd22906c0
skanderkaroui <skander.karoui@gmail.com>
2024-02-23 09:23:44 +0100

    false work

--- 3_arrays.py
+++ 3_arrays.py
@@ -1,8 +1,11 @@
 string ="abccddelkjr"
 
-def rem_dup(string):
-    set_string = set(string)
-    rm_string = "".join(set_string)
-    return rm_string
+# def rem_dup(string):
+#     set_string = set(string)
+#     rm_string = "".join(set_string)
+#     return rm_string
+
+#wrong solution because it uses set (additional buffer means either a set or a list)
+
 
 print(rem_dup(string))
