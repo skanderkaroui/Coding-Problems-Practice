9fed1ca8ab58e8708a8817dce182c1209c1782c9
skanderkaroui <skander.karoui@gmail.com>
2024-02-24 20:38:27 +0100

    balanced trees

---
+++ balanced_trees.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,data):
+        self.right = None
+        self.left = None
+        self.data = data
+    
+    def insert(self,data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def balanced(self):
+        if self.data:
+            if self.left and self.right:
+                balanced(self.data)
+        if (self.data - self.left == 1) or (self.right - self.data == 1):
+            return True
+        else:
+            return False
+
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+print(root.balanced(root))
+                
+            
