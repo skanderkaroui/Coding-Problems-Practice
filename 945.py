c5e2c6c6e1470121d4c704287ffa9dcdae0237d7
skanderkaroui <skander.karoui@gmail.com>
2024-06-15 00:40:31 +0100

    correct solution

--- 945.py
+++ 945.py
@@ -5,12 +5,11 @@ def minIncrementForUnique(nums):
     """
     nums.sort()
     count = 0
-    for i in range(len(nums)-1):
-        if nums[i] == nums[i+1]:
-            nums[i+1] += 1
-            count += 1
-
-    
+    for i in range(1, len(nums)):
+        if nums[i] <= nums[i-1]:
+            diff = nums[i - 1] - nums[i] + 1
+            count += diff
+            nums[i] = nums[i-1] + 1
     return count
 
 nums = [3,2,1,2,1,7]
