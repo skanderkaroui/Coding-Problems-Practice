18eba3638a187cc3c3bf42af1c8dac4321f378c9
skanderkaroui <skander.karoui@gmail.com>
2024-11-15 10:18:29 +0100

    optimized approach

--- 49.py
+++ 49.py
@@ -4,33 +4,16 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        def isAnagram(s, t):
-            if len(s) != len(t):
-                return False
-            
-            count = [0] * 26
+        res = {}
 
-            for i in range(len(s)):
-                count[ord(s[i]) - ord('a')] += 1
-                count[ord(t[i]) - ord('a')] -= 1
-            
-            for val in count:
-                if val != 0:
-                    return False
-            return True
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word not in res:
+                res[sorted_word] = []
+            res[sorted_word].append(word)
         
-        res = []
-        subset = []
+        return list(res.values())
 
-        for i in range(len(strs)):
-            subset.append[strs[i]]
-            if strs[i] not in res:
-                for j in range(i + 1, len(strs)):
-                    if isAnagram(strs[i], strs[j]):
-                        subset.append(strs[j])
-                res.append(subset)
-                subset = []
-        return res
                     
 obj = Solution()
 print(obj.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
