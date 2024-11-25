e115ad51e4b901779f8fedbfb63d052611141f29
skanderkaroui <skander.karoui@gmail.com>
2024-11-25 20:53:18 +0100

    update readme

---
+++ 125.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stripped_word = ''.join(c.lower() for c in s if c.isalnum())
+        l, r = 0, len(stripped_word) - 1
+        while l <= r:
+            if stripped_word[l] != stripped_word[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+obj = Solution()
+print(obj.isPalindrome("A man, a plan, a canal: Panama"))
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# LeetCode Problem Solutions
+
+This repository contains my solutions to various LeetCode problems. I maintain this repository to track my progress in algorithmic problem-solving and keep my coding skills sharp. Here is my [LeetCode Profile](https://leetcode.com/skirrrrrra]).
+## Progress Tracking
+
+| Category | Solved |
+|----------|---------|
+| Easy | 76 |
+| Medium | 88 |
+| Hard | 3 |
+| Total | 167 |
