9edcc4cd58e920ea1344653f0ff471a6c1759cd7
skanderkaroui <skander.karoui@gmail.com>
2025-02-15 17:12:45 +0100

    correct problems from last contest

--- B_The_special_paintbrush.py
+++ B_The_special_paintbrush.py
@@ -2,24 +2,7 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    s = input()
-    count = 0
-    start = True
-
-    for i in range(n):
-        if s[i] == "W":
-            if start == False:
-                continue
-            if start == True:
-                if i != n:
-                    count += 1
-                else:
-                    continue         
-        else:
-            if start == False:
-                start = True
-                count += 1
-            else:
-                count += 1
-    
-    print(count)
+    s = input().strip()
+    first = s.find('B')
+    last = s.rfind('B')
+    print(last - first + 1)
--- E_The_beautiful_String.py
+++ E_The_beautiful_String.py
@@ -1,18 +1,32 @@
-t = int(input())
-
+import sys
+data = sys.stdin.read().split()
+t = int(data[0])
+idx = 1
+res = []
 for _ in range(t):
-    s = input()
-    q = int(input())
-    s_list = list(s)
+    s = list(data[idx]); idx += 1
+    n = len(s)
+    q = int(data[idx]); idx += 1
+    occ = [False] * (n - 3) if n >= 4 else []
+    cnt = 0
+    for j in range(n - 3):
+        if s[j] == '1' and s[j+1] == '1' and s[j+2] == '0' and s[j+3] == '0':
+            occ[j] = True
+            cnt += 1
     for _ in range(q):
-        i, v = list(map(int,input().split()))
-
-        s_list[i-1] = str(v)
-
-        s = ''.join(s_list)
-
-        if "1100" in s:
-            print("YES")
-        else:
-            print("NO")
-
+        pos = int(data[idx]) - 1
+        new_val = data[idx + 1]
+        idx += 2
+        s[pos] = new_val
+        st = max(0, pos - 3)
+        ed = min(pos, n - 4)
+        for j in range(st, ed + 1):
+            valid = s[j] == '1' and s[j+1] == '1' and s[j+2] == '0' and s[j+3] == '0'
+            if valid and not occ[j]:
+                occ[j] = True
+                cnt += 1
+            elif not valid and occ[j]:
+                occ[j] = False
+                cnt -= 1
+        res.append("YES" if cnt else "NO")
+sys.stdout.write("\n".join(res))
