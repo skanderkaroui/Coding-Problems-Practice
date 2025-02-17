816d6199dad1e374ae5e37a7e38d5145f8531b20
skanderkaroui <skander.karoui@gmail.com>
2025-02-17 21:13:28 +0100

    correct implementation

--- A_Nathan_is_rich.py
+++ A_Nathan_is_rich.py
@@ -1,14 +1,5 @@
 t = int(input())
 
-def wheels_seen():
-        n = int(input())
-        n_2 = int(n / 2)
-        res = []
-
-        for c in range(n):
-            b = n_2 - 2 * c
-            if b >= 0:
-                return b + c
-                
 for _ in range(t):
-    print(wheels_seen())
+    n = int(input())
+    print((n + 2) //4 )
